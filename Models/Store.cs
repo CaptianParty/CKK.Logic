e643d2552c3dc5b6ae138ca9be2c9b14c6288487
Dominic Surnock <pot_mcsmokey2009@outlook.com>
2023-06-26 17:54:16 -0600

    changed using

--- Models/Store.cs
+++ Models/Store.cs
@@ -1,11 +1,4 @@
-﻿using CKK.Tests;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace CKK.Logic.Models
 {
     public class Store
